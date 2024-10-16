5c9998699dc826af11f6dfbb704c03d2e133e0a9
AlexaVas <alexandra.vasinova@gmail.com>
2024-10-16 14:41:06 +0200

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,10 +10,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <link
+        rel="icon"
+        type="image/png"
+        href="favicon/favicon-48x48.png"
+        sizes="48x48"
+      />
+      <link rel="icon" type="image/svg+xml" href="favicon/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="favicon/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="Blob" />
+      <link rel="manifest" href="favicon/site.webmanifest" />
 
-        {children}
-      </body>
+      <meta name="apple-mobile-web-app-title" content="Blob" />
+      <link rel="manifest" href="favicon/site.webmanifest" />
+      <body>{children}</body>
     </html>
   );
 }
